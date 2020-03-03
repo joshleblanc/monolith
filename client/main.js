147f895bb80ea2b6c2bb6af9e200dc9d2026cf5d
Joshua LeBlanc <jleblanc@omics.com>
2020-03-03 16:16:23 -0400

    chore: remove unused code

--- client/main.js
+++ client/main.js
@@ -8,12 +8,10 @@ app.use(svelte.router());
 
 app.path('/famtime', async ctx => {
   const { App } = await import("meteor/monolith:famtime/client/main.js");
-  console.log(App);
-  const a = new App({
+  new App({
     target: document.querySelector('main')
   });
-
-})
+});
 
 Meteor.startup(() => {
   app.load();
