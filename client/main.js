c090dd1283bf6526ca1db613a3d1f1ea9aaac865
Joshua LeBlanc <jleblanc@omics.com>
2020-03-03 16:15:18 -0400

    feat: routing for sub projects

--- client/main.js
+++ client/main.js
@@ -1,5 +1,20 @@
+import svelte from 'crayon-svelte';
+import crayon from 'crayon';
 
-Meteor.startup(async () => {
-    import("meteor/monolith:famtime/client/main.js");
 
+const app = crayon.create();
+
+app.use(svelte.router());
+
+app.path('/famtime', async ctx => {
+  const { App } = await import("meteor/monolith:famtime/client/main.js");
+  console.log(App);
+  const a = new App({
+    target: document.querySelector('main')
+  });
+
+})
+
+Meteor.startup(() => {
+  app.load();
 });
--- packages/famtime/client/main.js
+++ packages/famtime/client/main.js
@@ -1,4 +1,3 @@
-import App from './App.svelte';
 import 'bulma/css/bulma.css';
 import 'nes.css/css/nes.css';
 import '@fortawesome/fontawesome-pro/js/brands.js';
@@ -6,12 +5,4 @@ import '@fortawesome/fontawesome-pro/js/regular';
 import '@fortawesome/fontawesome-pro/js/fontawesome.js';
 import 'pickerjs/dist/picker.css';
 
-console.log("??");
-
-Meteor.startup(() => {
-
-  const a = new App({
-    target: document.querySelector('main')
-  });
-  console.log(a);
-});
+export App from './App.svelte';
