74f34c1447c467dea766977c4bc4f06d718e0bf5
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-28 12:46:45 -0300

    feat: add gnashblade

--- client/main.js
+++ client/main.js
@@ -1,16 +1,22 @@
 import svelte from 'crayon-svelte';
 import crayon from 'crayon';
-
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 const app = crayon.create();
 
 app.use(svelte.router());
 
-app.path('/famtime', async ctx => {
+app.path('/famtime/**', async ctx => {
   const { App } = await import("meteor/famtime/client/main.js");
   new App({
     target: document.querySelector('main')
   });
+});
+
+app.path('/gnashblade/**', async ctx => {
+  const { App } = await import("meteor/gnashblade/client/main.jsx")
+  ReactDOM.render(<App />, document.querySelector('main'));
 });
 
 Meteor.startup(() => {
--- server/main.js
+++ server/main.js
@@ -1,1 +1,2 @@
 import "meteor/famtime/server/main.js";
+import "meteor/gnashblade/server/main.js";
