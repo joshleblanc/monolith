08353d9920b8ea53700fe5cda167ee8358c856f3
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-02 21:16:05 -0300

    fix: use crayon native mounting

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import svelte from 'crayon-svelte';
-import ReactDOM from 'react-dom';
+import react from 'crayon-react';
 import React from 'react';
 import { app } from 'meteor/cereal:ui/lib/crayon';
 
 app.use(svelte.router());
+app.use(react.router());
 
 app.path('/famtime/**', async ctx => {
   const { App } = await import("meteor/famtime/client/main.js");
@@ -12,12 +13,12 @@ app.path('/famtime/**', async ctx => {
 
 app.path('/gnashblade/**', async ctx => {
   const { App } = await import("meteor/gnashblade/client/main.jsx")
-  ReactDOM.render(<App />, document.querySelector('main'));
+  ctx.mount(() => <App />)
 });
 
 app.path('/secret-santa/**', async ctx => {
   const { App } = await import("meteor/secret-santa/client/main.js");
-  ReactDOM.render(<App />, document.querySelector('main'));
+  ctx.mount(() => <App />)
 })
 
 Meteor.startup(() => {
